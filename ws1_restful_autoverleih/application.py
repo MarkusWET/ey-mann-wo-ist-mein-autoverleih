a4f7fbdbc7a99682bb5c22774b31cdf2702c9f3e
MarkusWET <markuswet@protonmail.com>
2018-04-15 14:42:39 +0200

    Set up skeleton for Unit Tests

--- ws1_restful_autoverleih/application.py
+++ ws1_restful_autoverleih/application.py
@@ -1,10 +1,5 @@
 #!/usr/bin/env python
-import os
 from app import create_app
-from app.data_models import Car, RentalHistory, User
-
-# from app.currency_exchange import convert_from_eur, convert_to_eur
-
 
 if __name__ == "__main__":
     application = create_app()
